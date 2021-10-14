6f7c98621928085b7a6dab284eb3d39cd1c8e38e
Andres Oliveira <andres.oliveira@zeeng.com.br>
2021-10-14 16:36:47 -0300

    Ex2-b

---
+++ js/ex2_10-b.js
@@ -0,0 +1,15 @@
+function calcularValor(){
+  var inValor = document.getElementById("inValor")
+  var inTempo = document.getElementById("inTempo")
+  var outValor = document.getElementById("outValor")
+
+  valor = Number(inValor.value)
+  tempo = Number(inTempo.value)
+
+  var pagar = Math.ceil(tempo / 15) * valor;
+
+  outValor.textContent = "Valor a Pagar R$: " + pagar
+}
+
+var btCalcular = document.getElementById("btCalcular")
+btCalcular.addEventListener("click", calcularValor)
